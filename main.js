e654a8c2ac2dd7cc5d2a55cb58f60ac68ffc82b3
StevieBrooks <brookscodeblog@gmail.com>
2023-07-13 14:39:30 +0700

    remove trouble and start work on style

--- main.js
+++ main.js
@@ -10,175 +10,10 @@ window.addEventListener("load", () => {
     // $("#countdownModal").modal("show");
     // $("#todoModal").modal("show");
     // $("#gymModal").modal("show");
-    $("#bleepModal").modal("show");
+    // $("#bleepModal").modal("show");
+    // $("#youtubeModal").modal("show");
 
 })
-
-
-/* BLEEP SECTION */
-
-$(".bleep-btn").click(function() {
-    $("#bleepModal").modal("show");
-})
-
-/* only 9 levels for demo purposes */
-const levelDetails = {
-    level1: {
-        shuttles: 7,
-        "time per shuttle (s)": 9,
-        "total level time (s)": 63,
-        "distance per level (m)": 140,
-        "cumulative distance (m)": 140
-    },
-    level2: {
-        shuttles: 8,
-        "time per shuttle (s)": 8,
-        "total level time (s)": 64,
-        "distance per level (m)": 160,
-        "cumulative distance (m)": 300
-    },
-    level3: {
-        shuttles: 8,
-        "time per shuttle (s)": 7.58,
-        "total level time (s)": 60.6,
-        "distance per level (m)": 160,
-        "cumulative distance (m)": 460
-    },
-    level4: {
-        shuttles: 9,
-        "time per shuttle (s)": 7.21,
-        "total level time (s)": 64.8,
-        "distance per level (m)": 180,
-        "cumulative distance (m)": 640
-    },
-    level5: {
-        shuttles: 9,
-        "time per shuttle (s)": 6.86,
-        "total level time (s)": 61.7,
-        "distance per level (m)": 180,
-        "cumulative distance (m)": 820
-    },
-    level6: {
-        shuttles: 10,
-        "time per shuttle (s)": 6.55,
-        "total level time (s)": 65.5,
-        "distance per level (m)": 200,
-        "cumulative distance (m)": 1020
-    },
-    level7: {
-        shuttles: 10,
-        "time per shuttle (s)": 6.26,
-        "total level time (s)": 62.6,
-        "distance per level (m)": 200,
-        "cumulative distance (m)": 1220
-    },
-    level8: {
-        shuttles: 11,
-        "time per shuttle (s)": 6,
-        "total level time (s)": 66,
-        "distance per level (m)": 220,
-        "cumulative distance (m)": 1440
-    },
-    level9: {
-        shuttles: 11,
-        "time per shuttle (s)": 5.76,
-        "total level time (s)": 63.4,
-        "distance per level (m)": 220,
-        "cumulative distance (m)": 1660
-    },
-}
-
-let time = 0;
-let bleepMils = 0;
-let bleepSecs = 0;
-let shuttleCount = 0;
-let levDeets = null;
-let bleepClock = null;
-let i = 0;
-let beep = new Audio('beep.wav');
-let beepNewLevel = new Audio('beepNewLevel.wav');
-
-$(".bleep-start").click(function() {
-    beepNewLevel.play();
-    $(".bleep-distance").css("display", "none");
-    $(".bleep-final-level").css("display", "none");
-    $(".bleep-level").css("display", "block");
-
-    levDeets = Object.entries(levelDetails);
-    console.log(levDeets[i]);
-
-    console.log(shuttleCount);
-    clearInterval(bleepClock);
-
-    
-    bleepClock = setInterval(function() {
-        $(".bleep-level").text(`Level: ${levDeets[i][0].slice(5)} / Shuttle: ${shuttleCount}`)
-        bleepMils++;
-        if(bleepMils == 100) {
-            bleepMils = 0;
-            bleepSecs++;
-        }
-        time = `${bleepSecs}.${bleepMils}`;
-        console.log(time);
-
-
-
-        if(time == levDeets[i][1]["time per shuttle (s)"]) {
-            bleepSecs = 0;
-            bleepMils = 0;
-            shuttleCount++;
-            beep.play();
-            console.log(shuttleCount);
-        }
-
-        if(shuttleCount == levDeets[i][1].shuttles) {
-            i++;
-            shuttleCount = 0;
-            beepNewLevel.play();
-            console.log(levDeets[i]);
-            $(".bleep-mot").css("display", "block");
-            $(".bleep-mot").text(`Congrats, onto level ${i + 1}`);
-            setTimeout(removeMot, 2000);
-        }
-    }, 10)   
-
-})
-
-$(".bleep-stop").click(function() {
-    $(".bleep-level").css("display", "none");
-    $(".bleep-distance").css("display", "block");
-    $(".bleep-final-level").css("display", "block");
-    setTimeout(resetTest, 1);
-   
-    
-    if(i < 1) {
-        $(".bleep-distance").text(`Total Distance: ${shuttleCount * 20}`);
-    } else {
-        let longerDistance = (levDeets[i - 1][1]["cumulative distance (m)"]) + (shuttleCount * 20);
-        $(".bleep-distance").text(`Total Distance: ${longerDistance}`);
-    }
-
-    $(".bleep-final-level").text(`Final Level: ${levDeets[i][0].slice(5)} / Shuttle: ${shuttleCount}`)
-})
-
-function resetTest() {
-    time = 0;
-    bleepMils = 0;
-    bleepSecs = 0;
-    shuttleCount = 0;
-    i = 0;
-    clearInterval(bleepClock);
-}
-
-function removeMot() {
-    $(".bleep-mot").css("display", "none");
-}
-
-
-
-
-
-
 
 
 /* STOPWATCH SECTION */
@@ -273,6 +108,7 @@ $("#stopwatchModal").on("hidden.bs.modal", function() {
     minutes = 0;
     $(".stopwatchDisplay").html("00 m : 00 s : 00 ms");
     });
+/* end of stopwatch section */
 
 
 /* COUNTDOWN SECTION */
@@ -459,6 +295,7 @@ $("#countdownModal").on("hidden.bs.modal", function() {
         - Add motivational message box underneath. Use for intervals of 5 minutes and 15 second intervals for last 90secs
     
     */
+/* end of countdown section */
 
 
 /* TO-DO SECTION */
@@ -521,136 +358,310 @@ tdModalBody.on("click", ".td-done", function(e) {
 tdModalBody.on("click", ".td-delete", function(e) {
     e.target.parentElement.parentElement.parentElement.remove();
 })
+/* end of todo list section */
+
+
+/* BLEEP SECTION */
+
+$(".bleep-btn").click(function() {
+    $("#bleepModal").modal("show");
+})
+
+/* only 9 levels for demo purposes */
+const levelDetails = {
+    level1: {
+        shuttles: 7,
+        "time per shuttle (s)": 9,
+        "total level time (s)": 63,
+        "distance per level (m)": 140,
+        "cumulative distance (m)": 140
+    },
+    level2: {
+        shuttles: 8,
+        "time per shuttle (s)": 8,
+        "total level time (s)": 64,
+        "distance per level (m)": 160,
+        "cumulative distance (m)": 300
+    },
+    level3: {
+        shuttles: 8,
+        "time per shuttle (s)": 7.58,
+        "total level time (s)": 60.6,
+        "distance per level (m)": 160,
+        "cumulative distance (m)": 460
+    },
+    level4: {
+        shuttles: 9,
+        "time per shuttle (s)": 7.21,
+        "total level time (s)": 64.8,
+        "distance per level (m)": 180,
+        "cumulative distance (m)": 640
+    },
+    level5: {
+        shuttles: 9,
+        "time per shuttle (s)": 6.86,
+        "total level time (s)": 61.7,
+        "distance per level (m)": 180,
+        "cumulative distance (m)": 820
+    },
+    level6: {
+        shuttles: 10,
+        "time per shuttle (s)": 6.55,
+        "total level time (s)": 65.5,
+        "distance per level (m)": 200,
+        "cumulative distance (m)": 1020
+    },
+    level7: {
+        shuttles: 10,
+        "time per shuttle (s)": 6.26,
+        "total level time (s)": 62.6,
+        "distance per level (m)": 200,
+        "cumulative distance (m)": 1220
+    },
+    level8: {
+        shuttles: 11,
+        "time per shuttle (s)": 6,
+        "total level time (s)": 66,
+        "distance per level (m)": 220,
+        "cumulative distance (m)": 1440
+    },
+    level9: {
+        shuttles: 11,
+        "time per shuttle (s)": 5.76,
+        "total level time (s)": 63.4,
+        "distance per level (m)": 220,
+        "cumulative distance (m)": 1660
+    },
+}
+
+let time = 0;
+let bleepMils = 0;
+let bleepSecs = 0;
+let shuttleCount = 0;
+let levDeets = null;
+let bleepClock = null;
+let i = 0;
+let beep = new Audio('beep.wav');
+let beepNewLevel = new Audio('beepNewLevel.wav');
+
+$(".bleep-start").click(function() {
+    beepNewLevel.play();
+    $(".bleep-distance").css("display", "none");
+    $(".bleep-final-level").css("display", "none");
+    $(".bleep-level").css("display", "block");
+
+    levDeets = Object.entries(levelDetails);
+    console.log(levDeets[i]);
+
+    console.log(shuttleCount);
+    clearInterval(bleepClock);
+
+    
+    bleepClock = setInterval(function() {
+        $(".bleep-level").text(`Level: ${levDeets[i][0].slice(5)} / Shuttle: ${shuttleCount}`)
+        bleepMils++;
+        if(bleepMils == 100) {
+            bleepMils = 0;
+            bleepSecs++;
+        }
+        time = `${bleepSecs}.${bleepMils}`;
+        console.log(time);
+
+
+
+        if(time == levDeets[i][1]["time per shuttle (s)"]) {
+            bleepSecs = 0;
+            bleepMils = 0;
+            shuttleCount++;
+            beep.play();
+            console.log(shuttleCount);
+        }
+
+        if(shuttleCount == levDeets[i][1].shuttles) {
+            i++;
+            shuttleCount = 0;
+            beepNewLevel.play();
+            console.log(levDeets[i]);
+            $(".bleep-mot").css("display", "block");
+            $(".bleep-mot").text(`Congrats, onto level ${i + 1}`);
+            setTimeout(removeMot, 2000);
+        }
+    }, 10)   
+
+})
+
+$(".bleep-stop").click(function() {
+    $(".bleep-level").css("display", "none");
+    $(".bleep-distance").css("display", "block");
+    $(".bleep-final-level").css("display", "block");
+    setTimeout(resetTest, 1);
+   
+    
+    if(i < 1) {
+        $(".bleep-distance").text(`Total Distance: ${shuttleCount * 20}`);
+    } else {
+        let longerDistance = (levDeets[i - 1][1]["cumulative distance (m)"]) + (shuttleCount * 20);
+        $(".bleep-distance").text(`Total Distance: ${longerDistance}`);
+    }
+
+    $(".bleep-final-level").text(`Final Level: ${levDeets[i][0].slice(5)} / Shuttle: ${shuttleCount}`)
+})
+
+function resetTest() {
+    time = 0;
+    bleepMils = 0;
+    bleepSecs = 0;
+    shuttleCount = 0;
+    i = 0;
+    clearInterval(bleepClock);
+}
+
+function removeMot() {
+    $(".bleep-mot").css("display", "none");
+}
+/* end of bleep test section */
 
 
 /* GYM SEARCH SECTION */
 
-let userLat = null;
-let userLong = null;
-// let showPosition = null;
+// let userLat = null;
+// let userLong = null;
+// // let showPosition = null;
 
-$(".gym-btn").click(function() {
-    $("#gymModal").modal("show");
+// $(".gym-btn").click(function() {
+//     $("#gymModal").modal("show");
 
-    var x = document.getElementById("demo");
+//     var x = document.getElementById("demo");
 
-    function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-    }
-    getLocation();
+//     function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+//     }
+//     getLocation();
 
-    function showPosition(position) {
-        userLat = position.coords.latitude;
-        userLong = position.coords.longitude;
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-    /* need to figure out how to extract this data and use in ajax call. console log below aint working */
-    makeAjaxRequest(userLat, userLong);
-    }
+//     function showPosition(position) {
+//         userLat = position.coords.latitude;
+//         userLong = position.coords.longitude;
+//     x.innerHTML = "Latitude: " + position.coords.latitude +
+//     "<br>Longitude: " + position.coords.longitude;
+//     /* need to figure out how to extract this data and use in ajax call. console log below aint working */
+//     makeAjaxRequest(userLat, userLong);
+//     }
 
-    function makeAjaxRequest(userLat, userLong) {
-        const settings = {
-            async: true,
-            crossDomain: true,
-            url: `gymSearch.php`,
-            method: 'GET',
-        };
+//     function makeAjaxRequest(userLat, userLong) {
+//         const settings = {
+//             async: true,
+//             crossDomain: true,
+//             url: `gymSearch.php`,
+//             method: 'GET',
+//         };
         
-        $.ajax(settings).done(function (response) {
-            let gymResponse = JSON.parse(response);
-            console.log(gymResponse);
-        });
-    }
+//         $.ajax(settings).done(function (response) {
+//             let gymResponse = JSON.parse(response);
+//             console.log(gymResponse);
+//         });
+//     }
 
-})
+// })
 
 
 /*YOUTUBE SECTION*/
-const youtubeBtn = document.querySelector(".youtube-btn");
-const youtubeModal = document.querySelector("#youtubeModal");
-const muscleArray = document.querySelectorAll(".ytdd-item");
-let muscleChoice = null;
-let carouselInner = document.querySelector(".carousel-inner");
-let ytItem = null; 
-let carouselArr = [];
+/* return to this later */
 
 
+// const youtubeBtn = document.querySelector(".youtube-btn");
+// const youtubeModal = document.querySelector("#youtubeModal");
+// const muscleArray = $(".ytdd-item");
+//   let muscleChoice = null;
+//   let carouselInner = $(".carousel-inner");
+//   let ytItem = null;
+//   let carouselArr = null;
 
-youtubeBtn.addEventListener("click", () => {
-    $("#youtubeModal").modal("show");
+// youtubeBtn.addEventListener("click", () => {
+//     $("#youtubeModal").modal("show");
 
-    Object.values(muscleArray).forEach(item => {
-        item.addEventListener("click", () => {
-            muscleChoice = item.innerHTML;
+//     Object.values(muscleArray).forEach(item => {
+//         item.addEventListener("click", () => {
+            
+//             $(".dropdown-toggle").text(item.innerHTML);
+//             muscleChoice = item.innerHTML;
 
-            const xhr = new XMLHttpRequest();
+//             const xhr = new XMLHttpRequest();
         
-            xhr.open('GET', `https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${muscleChoice}%20workout`);
+//             xhr.open('GET', `https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${muscleChoice}%20workout`);
         
-            xhr.addEventListener('readystatechange', function () {
-                if (this.readyState === this.DONE) {
-                    const res = JSON.parse(this.responseText);
-                    console.log(res);
-                    if(res.items.length == 0) {
-                        console.log("Apologies, no data right now.")
-                    } else {                     
-                        res.items.forEach(item => {
-                            carouselArr.push(item);
-                        })
-                    }
+//             xhr.addEventListener('readystatechange', function () {
+//                 if (this.readyState === this.DONE) {
+//                     carouselArr = [];
+//                     const res = JSON.parse(this.responseText);
+//                     console.log(res);
+//                     if(res.items.length == 0) {
+//                         console.log("Apologies, no data right now.")
+//                     } else {                     
+//                         res.items.forEach(item => {
+//                             carouselArr.push(item);
+//                         })
+//                     }
 
-                    console.log(carouselArr);
+//                     console.log(carouselArr);
 
-                    carouselArr.forEach(item => {
-                        if(item.type == 'video') {
+//                     carouselArr.forEach(item => {
+//                         if(item.type == 'video') {
 
-                            ytItem = document.querySelector(".yt-item:nth-child(2)");
-                            const cloneItem = ytItem.cloneNode(true);
-                            carouselInner.appendChild(cloneItem);
+//                             carouselInner.append().html(`
+//                             <div class="carousel-item yt-item">
+//                             <a href="${item.url}" target="_blank"><img src="${item.thumbnails[0].url}" class="d-block w-100" alt="..."></a>
+//                             <a href="${item.url}" target="_blank">
+//                               <div class="carousel-caption d-md-block bg-primary">
+//                                 <h5></h5>
+//                                 <span class="yt-duration">Duration: ${item.duration}</span>
+//                                 <br>
+//                                 <span class="yt-views">Views: ${item.views}</span>
+//                                 <br>
+//                                 <span class="yt-upload">Uploaded: ${item.uploadedAt}</span>
+//                               </div>
+//                             </a>
+//                           </div>
+//                             `);
 
-                            cloneItem.children[0].href = item.url;
-                            cloneItem.children[0].children[0].src = item.thumbnails[0].url;
-                            cloneItem.children[1].href = item.url;
-                            cloneItem.children[1].children[0].children[1].innerHTML += item.duration;
-                            cloneItem.children[1].children[0].children[3].innerHTML += item.views;
-                            cloneItem.children[1].children[0].children[5].innerHTML += item.uploadedAt;
+//                             ytItem = document.querySelector(".yt-item:nth-child(2)");
+//                             const cloneItem = ytItem.cloneNode(true);
+//                             carouselInner.appendChild(cloneItem);
 
 
 
-                                                        // Object.keys(item).forEach(key => {
-                            //     if(key == "thumbnails") {
-                            //         console.log("you can use this item");
-                            //     }
-                            // })
+
+//                                                         Object.keys(item).forEach(key => {
+//                                 if(key == "thumbnails") {
+//                                     console.log("you can use this item");
+//                                 }
+//                             })
     
 
-                        }
-                    })
-                    console.log(carouselInner.children);
+//                         }
+//                     })
+//                     console.log(carouselInner);
 
-                }
-            });
+//                 }
+//             });
         
-            xhr.setRequestHeader('X-RapidAPI-Key', 'cd323733eemsh2260acaef042b68p173ddajsn26dd1647e98f');
-            xhr.setRequestHeader('X-RapidAPI-Host', 'youtube-search-results.p.rapidapi.com');
-            /* NEED TO FIGURE OUT TO HIDE API KEY FROM NOW ON - VERY IMPORTANT!!! 
-            use this - https://www.freecodecamp.org/news/how-to-securely-store-api-keys-4ff3ea19ebda/
-            */
+//             xhr.setRequestHeader('X-RapidAPI-Key', 'cd323733eemsh2260acaef042b68p173ddajsn26dd1647e98f');
+//             xhr.setRequestHeader('X-RapidAPI-Host', 'youtube-search-results.p.rapidapi.com');
         
-            xhr.send();
-        })
-    })
+//             xhr.send();
+//         })
+//     })
 
     
 
 
 
 
+// })
 
-    // ytItem1.children[0].attributes[0].value = "https://www.youtube.com/";
-    // ytItem1.children[1].attributes[0].value = "https://www.youtube.com/";
-})
+// $("#youtubeModal").on("hidden.bs.modal", function() {
+//     $(".dropdown-toggle").text("Select Muscle Group");
+//     });
